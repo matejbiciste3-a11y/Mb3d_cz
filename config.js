059ceba22ab12dbd92f9d03458120befec053dbd
Matej Biciste <matejbiciste3@gmail.com>
2026-07-10 17:18:38 +0200

    Oprava supabase.auth

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ const CONFIG = {
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlcnJraHVwZ2Nyb29zZnR0aHJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2ODAwMTUsImV4cCI6MjA5OTI1NjAxNX0.eO2_zCDjlzDs-Ijj-VMqXh_SP2A9YpcDizFIXoY6DyY'
 };
 
-window.supabase = window.supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
+const supabase = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
 
-console.log('✅ Supabase inicializován:', window.supabase);
-console.log('✅ Supabase.auth existuje?', !!window.supabase.auth);
+window.supabaseClient = supabase;
+
+console.log('✅ Supabase inicializován:', supabase);
+console.log('✅ Supabase.auth existuje?', !!supabase.auth);
